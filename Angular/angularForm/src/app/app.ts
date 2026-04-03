3b4407a77debb1ddb7448a41495a96667ccf95fc
Thirisha-C <thirisha@sparkouttech.com>
2026-04-03 19:01:29 +0530

    add task

--- Angular/angularForm/src/app/app.ts
+++ Angular/angularForm/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Login } from './Forms/login/login';
 import { RegisterForm } from './Forms/register-form/register-form';
-
+import { Registration } from './Forms/registration/registration';
+import { StudentForm } from './Forms/student-form/student-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, RegisterForm],
+  imports: [Login, RegisterForm, Registration,StudentForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
